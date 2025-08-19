be81a553e44132b1fa750d01a5aecd6e71654507
Deepak kumar <nkrajoriya33@gmail.com>
2025-08-19 12:35:05 +0530

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+name: smart_hisab
+description: Offline accounting app (minimal working build)
+publish_to: 'none'
+version: 0.1.0+1
+
+
+environment:
+sdk: ">=3.3.0 <4.0.0"
+
+
+dependencies:
+flutter:
+sdk: flutter
+cupertino_icons: ^1.0.6
+sqflite: ^2.3.3+1
+path_provider: ^2.1.3
+url_launcher: ^6.3.0
+intl: ^0.19.0
+pdf: ^3.11.0
+printing: ^5.13.4
+collection: ^1.18.0
+
+
+flutter:
+uses-material-design: true
+assets:
+- assets/images/logo.png
+  
